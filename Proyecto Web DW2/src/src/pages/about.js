5423ee6e7f5f521b583cf6f889db069749750b0d
melvin-6 <motejada2000@gmail.com>
2024-12-02 13:05:45 -0600

    about.js

---
+++ Proyecto Web DW2/src/src/pages/about.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className="about">
+            <h2>Sobre Mí</h2>
+            <p>
+                Soy un joven que le gusta practicar deportes, jugar videojuegos y siempre con ganas de aprender.
+                Tengo experiencia en realizar mantenimientos a equipos farmaceuticos. 
+                Juego LOL y EAFC xd.
+            </p>
+            <p>
+                Me especializo en mantenimientos preventivos y correctivos  
+                en equipos industriales tanto en el area farmaceutica como de plastico  
+                siempre me llamo la antencion la programacion y poderla aplicar en la industria para poder 
+                tener un entorno en constante evolución.
+            </p>
+        </section>
+    );
+}
+
+export default About;
